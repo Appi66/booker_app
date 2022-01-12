80090a3cf97802ac70d50afc25b8e556662e81e9
Appi66 <96176617+Appi66@users.noreply.github.com>
2022-01-12 10:19:45 +0000

    [Add]投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'bookers/new'
-  get 'bookers/index'
+  root to: 'homes#top'
+  get 'books/new'
+  get 'books/index'
   post 'books' => 'books#create'
-  get 'bookers/show'
-  get 'bookers/edit'
-  get '/top' => "homes#top"
-  get 'books' => "bookers#index"
+  get 'books/show'
+  get 'books/edit'
+  get '/top' => 'homes#top'
+  get 'books' => 'books#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
