0dc9984ecdefc45a544a14bb8a5b91b8fe2b17ec
Appi66 <96176617+Appi66@users.noreply.github.com>
2022-01-12 05:41:25 +0000

    [Update]モデル名の変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'homes/top'
   get 'bookers/new'
   get 'bookers/index'
   get 'bookers/show'
   get 'bookers/edit'
   get '/top' => "homes#top"
+  get 'books' => "bookers#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
