eff0707d007c6aabf5140ff659313aee2aa60dcc
Appi66 <96176617+Appi66@users.noreply.github.com>
2022-01-12 12:30:23 +0000

    [Add]詳細画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get 'books/new'
-  get 'books/index'
   post 'books' => 'books#create'
-  get 'books/show'
   get 'books/edit'
   get '/top' => 'homes#top'
   get 'books' => 'books#index'
+  get 'books/:id' => 'books#show'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
