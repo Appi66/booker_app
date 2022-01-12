2154775640470b928b430096551df081228aa87c
Appi66 <96176617+Appi66@users.noreply.github.com>
2022-01-12 15:45:25 +0000

    [Add]編集機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to show_path(params[:id])
+    redirect_to books_path(@book.id)
   end
 
   def show
@@ -18,7 +18,15 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to books_path(@book.id)
+  end
+
 
 
   private
