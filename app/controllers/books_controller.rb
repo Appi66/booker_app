6b12a9d9d0e0965206d6aec236ea1cb5931b6d25
Appi66 <96176617+Appi66@users.noreply.github.com>
2022-01-13 12:20:33 +0000

    [Update]フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,7 +30,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   if @book.update(book_params)
     flash[:notice] = "Book was successfully updated."
-    redirect_to books_path(book.id)
+    redirect_to book_path(@book.id)
   else
     render :edit
   end
@@ -39,6 +39,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
 
